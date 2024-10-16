49ca4517fb1dbd1ee62016f11cb8e1750a1d6b49
Christiaan <jeffjefferson0502@gmail.com>
2024-10-16 08:47:12 +0200

    General update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,16 +3,20 @@
 --
 -- See the kickstart.nvim README for more information
 
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 return {
-	{ 
+	{
 	"ellisonleao/gruvbox.nvim", priority = 1000 , config = function()
 		vim.o.background = "dark",
 		vim.cmd([[colorscheme gruvbox]])
 		end
 	},
-	
-	{
+
+  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
@@ -25,5 +29,8 @@ return {
       },
     },
   },
+
+  { 'echasnovski/mini.icons', version = false },
+  { 'nvim-tree/nvim-web-devicons', },
 
 }
